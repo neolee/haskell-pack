dba5d6a2cc02eeaf2258f3cca004a41e5eb157df
Neo Lee <neo.lee@gmail.com>
2014-08-04 16:57:14 +0800

    Add structured-haskell-mode (disabled by default)

--- config/haskell-conf.el
+++ config/haskell-conf.el
@@ -1,4 +1,8 @@
 (live-add-pack-lib "haskell-mode")
 (require 'haskell-mode-autoloads)
-
 (add-to-list 'Info-default-directory-list "../lib/haskell-mode/")
+
+;; structured-haskell-mode disabled by default
+; (live-add-pack-lib "structured-haskell-mode")
+; (require 'shm)
+; (add-hook 'haskell-mode-hook 'structured-haskell-mode)
